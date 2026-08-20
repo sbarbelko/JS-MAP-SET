329b16396866b6b480df887efdfd1b03ad0fa66f
sbarbelko <s.barbelko63@gmail.com>
2026-08-20 14:04:25 +0300

    Complete 2.2

--- assets/js/Task2.js
+++ assets/js/Task2.js
@@ -16,8 +16,21 @@ for (const value of collection) {
 }
 console.log("sum", sum);
 
-collection.delete(2)
-console.log(collection)
+collection.delete(2);
+console.log(collection);
 
 collection.clear();
-console.log(collection)
+console.log(collection);
+
+// 2.2
+
+const arr = [1, 6, 9, 4, 9, 1, 5, 6];
+
+const uniqueArr = [...new Set(arr)];
+console.log(uniqueArr);
+
+const arr1 = [1, 6, 9, 4, 9, 1, 5, 6];
+const arr2 = [5, 10, 11];
+
+const uniqueTwoArr = [...new Set([...arr1, ...arr2])];
+console.log(uniqueTwoArr);
