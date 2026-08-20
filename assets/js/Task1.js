6d4f021857932b3c2af56582c69d94a844ce9918
sbarbelko <s.barbelko63@gmail.com>
2026-08-20 13:46:53 +0300

    Complete 1.2

--- assets/js/Task1.js
+++ assets/js/Task1.js
@@ -19,3 +19,18 @@ console.log(arrMap.has(2));
 for (const [key, value] of arrMap) {
   console.log(`${key} - ${value}`);
 }
+
+// 1.2
+arrMap.set(1, "first");
+function numToText(text) {
+  return text
+    .split(" ")
+    .map((word) => (arrMap.has(Number(word)) ? arrMap.get(Number(word)) : word))
+    .join(" ");
+}
+
+const text =
+  "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.";
+
+console.log(text);
+console.log(numToText(text));
